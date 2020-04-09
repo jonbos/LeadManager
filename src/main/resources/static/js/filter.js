d003574c13e8d2de109b85f56648e298ba1c416d
Jon Bos <bos.jon@gmail.com>
2020-04-09 13:54:18 -0500

    refactor: use jQuery for table filter script

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -1,22 +1,8 @@
-function myFunction() {
-	// Declare variables
-	var input, filter, table, tr, td, i, txtValue;
-	input = document.getElementById("searchinput");
-	filter = input.value.toUpperCase();
-	table = document.getElementById("leads-table");
-	tr = table.getElementsByTagName("tr");
-
-	// Loop through all table rows, and hide those who don't match the search
-	// query
-	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td")[0];
-		if (td) {
-			txtValue = td.textContent || td.innerText;
-			if (txtValue.toUpperCase().indexOf(filter) > -1) {
-				tr[i].style.display = "";
-			} else {
-				tr[i].style.display = "none";
-			}
-		}
-	}
-}
+$(document).ready(function() {
+	$("#searchinput").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$("#leads-table tbody tr").filter(function() {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});		
+	});
+});
